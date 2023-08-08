b1132fe12d612d53b86322c6d8a08a6f139240ed
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-08-08 16:37:30 -0400

    Wrote logic for weight ranges

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ import "regenerator-runtime/runtime";
 // VARIABLES
 const metricHeightInput = document.getElementsByName("centimeters")[0];
 const metricWeightInput = document.getElementsByName("kilograms")[0];
-let height = null;
-let weight = null;
 
-// HELPER FUNCTION
-function isValidHeightandWeight() {
+// HELPER FUNCTIONS
+function isValidHeightandWeight(height, weight) {
   const conditions = [
     typeof height === "number",
     typeof weight === "number",
@@ -19,17 +17,53 @@ function isValidHeightandWeight() {
   return conditions.every((condition) => condition);
 }
 
+function categorizeBMI(bmi) {
+  let category;
+
+  switch (true) {
+    case bmi < 18.5:
+      category = "underweight";
+      break;
+    case bmi < 24.9:
+      category = "a healthy weight";
+      break;
+    case bmi < 29.9:
+      category = "overweight";
+      break;
+    default:
+      category = "obese";
+  }
+
+  return category;
+}
+
+function calculateIdealWeightRange(height) {
+  const minBMI = 18.5;
+  const maxBMI = 24.9;
+
+  const minIdealWeight = ((minBMI * (height * height)) / 10000).toFixed(1);
+  const maxIdealWeight = ((maxBMI * (height * height)) / 10000).toFixed(1);
+
+  return {
+    minIdealWeight: minIdealWeight,
+    maxIdealWeight: maxIdealWeight,
+  };
+}
+
 // EVENT LISTENER CALLBACK FUNCTION
 function calculateBMI() {
   height = parseFloat(metricHeightInput.value);
   weight = parseFloat(metricWeightInput.value);
 
-  isValidHeightandWeight();
-  if (!isValidHeightandWeight) return;
-  const bmi = weight / ((height * height) / 10000);
-  console.log(bmi);
+  if (!isValidHeightandWeight(height, weight)) return;
+  const bmi = (weight / ((height * height) / 10000)).toFixed(1);
+  const category = categorizeBMI(bmi);
+  const { minIdealWeight, maxIdealWeight } = calculateIdealWeightRange(height);
 }
 
 // EVENT LISTENERS
 metricHeightInput.addEventListener("input", calculateBMI);
 metricWeightInput.addEventListener("input", calculateBMI);
+
+// AFTER BMI IS CALCULATED
+// WE WANT TO HIDE WELCOME, DISPLAY RESULT
